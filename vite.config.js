beaad1a935ffa7ced5149bb360a3e80211def90f
John Masseria <jmasseri@mdc.edu>
2026-05-26 17:18:46 -0400

    Correct vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/my-lecture-slides/",
   plugins: [react()],
 })
-
-export default {
-  base: "/my-lecture-slides/",   // must match your GitHub repo name exactly
-}
